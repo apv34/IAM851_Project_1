512b01ce173f5b5b7f37538803b38f9fb2b48061
Anthony Edmonds <apv34@fishercat.sr.unh.edu>
2014-04-15 13:19:07 -0400

    4/15/2014 - Added Four new tests: Compound waves, RK4 scaling with increasing numbers of points. Added draft of Code Documentation.

---
+++ test_kdv_comp.c
@@ -0,0 +1,72 @@
+
+#include <stdio.h>
+#include "kdv_equation.h"
+#include "solver.h"
+
+void initial( Vector *u0, double dx )
+{
+    int i;
+    for( i = 0; i < u0 -> N; i++ )
+        VEC(u0,i) = 2/ pow(cosh(dx*i-2), 2)+ 2/ pow(cosh(dx*i-6), 2);
+}
+
+int main()
+{
+    Vector u;
+    Vector u_n;
+    
+    int steps = 10000;
+    int N = 128;
+    double dx = 8/((double)N-1);
+    double dt = pow(dx, 3);
+    double beg, end;
+    int i = 0;
+    int k = 0;
+    int write = 10;
+    int valid;
+    FILE* output;
+
+    printf( "Test KdV Two Waves\n" );
+    beg = WTime();
+    vector_initialize( &u, N,
+        (double[MAX_SIZE]) {0,0}, MAX_SIZE );
+    vector_initialize( &u_n, N,
+        (double[MAX_SIZE]) {0,0}, MAX_SIZE );
+    initial( &u, dx);
+    output = fopen( "comp_wave.txt", "w" );
+    fclose( output );
+    end = WTime();
+    printf( "Set-up took: %fs\n", end-beg);
+    beg = WTime();
+    while( i < steps )
+    {
+        if( k % write == 0 )
+        {
+            vector_write( &u, "comp_wave.txt" );
+            i++;
+            if( i / (double) steps == .25 )
+                printf( "25%% Completed\n" );
+            else if(  i / (double) steps == .50 )
+                printf( "50%% Completed\n" );
+            else if(  i / (double) steps == .75 )
+                printf( "75%% Completed\n" );
+        }
+        k++;
+        valid = runge_kutta( dt, &u, dx, &u_n, pdu_pdt);
+        vector_copy( &u, &u_n);
+        if( valid == -1 )
+        {
+            printf("ERROR!\n");
+            printf( "%f\n", k*dt);
+            assert( valid != -1 );
+            break;
+        }
+    }
+    end = WTime();
+    
+    printf( "RK4 with %d Points written every %d steps", N, write);
+    printf( " for %d Steps took: %fs\n", steps, end-beg);
+    printf( "For a simulation of %fs\n", k*dt);
+    
+    return 0;
+}
---
+++ test_kdv_scaling.c
@@ -0,0 +1,55 @@
+
+#include <stdio.h>
+#include "kdv_equation.h"
+#include "solver.h"
+
+#define RUN 7
+
+int main()
+{
+    Vector u;
+    Vector u_n;
+    
+    int N = 32;
+    double dx = 8/((double)N-1);
+    double dt = pow(dx, 3);
+    double beg, end;
+    double time[RUN] = { 0.0 };
+    int k = 0;
+    int i = 0;
+    int valid;
+
+    printf( "Test KdV Scaling\n" );
+    for( i = 0; i < RUN; i++ ){
+        N = N*2;
+        dx = 8/((double)N-1);
+        dt = pow(dx, 3);
+        printf( "Run %d for N = %d\n", i+1,N);
+        vector_initialize( &u, N,
+            (double[MAX_SIZE]) {0,0}, MAX_SIZE );
+        vector_initialize( &u_n, N,
+            (double[MAX_SIZE]) {0,0}, MAX_SIZE );
+        simple_sec( &u, dx, 1, 4 );
+        beg = WTime();
+        for( k = 0; k*dt < 4; k++ )
+        {
+            valid = runge_kutta( dt, &u, dx, &u_n, pdu_pdt);
+            vector_copy( &u, &u_n);
+            if( valid == -1 )
+            {
+                printf("ERROR!\n");
+                printf( "%f\n", k*dt);
+                assert( valid != -1 );
+                break;
+            }
+        }
+        end = WTime();
+        time[i] = end - beg;
+    }
+    printf( "KdV solution of 2*sech^2(x+4) for 4s:\n" );
+    for( i = 0; i < RUN; i++ ){
+        printf( "N = %6f  Took: %fs\n", 128*pow(2, i ), time[i]);
+    }
+    
+    return 0;
+}
