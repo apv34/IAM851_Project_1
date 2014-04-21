ecaa5c711bacc08db79ce1584bdfc75f9beb8852
Anthony Edmonds <apv34@fishercat.sr.unh.edu>
2014-04-21 18:33:37 -0400

    4/20/2014 -- Work attemping to fix conversion program segfault.

--- convert.c
+++ convert.c
@@ -2,67 +2,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAX 100000
-int main( int argc, char *argv[]  )
+#define MAX_N 100000
+int main(int argc, char *argv[] )
 {
-    int rows = 0;
-    int cols = 0;
-    double data[MAX][MAX] = { { 0.0 } };
+    int rows = 10;
+    int cols = 10;
+    double data[MAX_N][MAX_N] = { { 1.0, 2.0, 0.0, 0.0 } };
     FILE *rotate;
     int i = 0;
     int j = 0;
     
-    printf( "A\n" );
-    if( argc != 4 ) 
+
+    if( argc == 4 )
     {
-        printf( "Incorrect number of passed arguments!\n" );
-        return 0;
-    }
-    else
-    {
-        printf( "B\n" );
         rows = atoi(argv[2] );
-        cols = atoi(argv[3] );
-        printf( "C\n" );
-        if( rows > MAX || cols > MAX )
-        {
-            printf( "Number of Rows/Columns Exceeds Maximum"
-                " Allowable!\n" );
-            return 0;
-        }
-        printf( "D\n" );
+        cols = atoi( argv[3] );
+        printf( "Opening %s\nWhich should have %d rows and %d Columns\n", argv[1], rows, cols);
         rotate = fopen( argv[1], "r" );
-        printf( "E\n" );
-        if( rotate == NULL)
+        if( rotate == NULL ) 
         {
-            printf( "File could not be opened!" );
-            return 0;
+            printf( "%s is an invalid file name!\n", argv[1] );
         }
         else
         {
-            printf( "F\n" );
-            printf( "R%d\nC%d\n", rows, cols );
-            for( i = 0; i < rows; i++ ){
-                for( j = 0; j < cols; j++ ){
-                    fscanf(rotate, "%lf", &data[i][j] );
+            for( i = 0; i < rows; i++ )
+            {
+                for( j = 0; j < cols; j++ )
+                {
+                    fscanf( rotate, "%lf", &data[i][j] );
                 }
             }
-            printf( "G\n" );
             fclose(rotate);
-            rotate = fopen( argv[1], "w" );
-            printf( "H\n" );
-            for( j = 0; j < cols; j++ ){
-                for( i = 0; i < rows; i++ ){
-                    fprintf( rotate, "%f ", data[i][j] );
-                }
+            rotate = fopen( "rotated.txt", "w" );
+            printf( "B\n" );
+            for( j = 0; j < cols; j++ )
+            {
+                for( i = 0; i < rows; i++ )
+                    fprintf( rotate,"%f ", data[i][j] );
                 fprintf( rotate, "\n" );
             }
-            printf( "J\n" );
+            fclose(rotate);
             
+            printf( "C\n" );
         }
-        fclose(rotate);
     }
-
+    else
+    {
+        printf( "Incorrect number of arguments!\n" );
+    }
     
     return 0;
-}
+};
