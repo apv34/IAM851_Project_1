430a940a8fa4a977fa2c4c3e794dfeb2c48aab77
Anthony Edmonds <apv34@fishercat.sr.unh.edu>
2014-04-21 10:24:53 -0400

    4/21/2014  -- Segfault was fixed. Removal of print statements.

--- kdv_equation.c
+++ kdv_equation.c
@@ -168,9 +168,7 @@ void print_timing( double numSteps, double dt, int writeEvery,
     int k = 0;
     FILE* output;
     
-    printf( "A\n" );
     output = fopen( file_name, "w" );
-    printf( "B\n" );
     // Time
     while( i < numSteps)
     {
@@ -180,7 +178,6 @@ void print_timing( double numSteps, double dt, int writeEvery,
         }
         k++;
     }
-    printf( "C\n" );
     fprintf( output, "\n" );
     // x
     for( i = 0; (left+i*dx) < right; i++ )
@@ -188,7 +185,5 @@ void print_timing( double numSteps, double dt, int writeEvery,
         fprintf( output, "%f ", left+i*dx );
     }
     fprintf( output, "\n" );
-    printf( "D\n" );
     fclose(output);
-    printf( "E\n" );
 }
