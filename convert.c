5e07c2a93bbf328e56dd6c045de9f474f0827287
Anthony Edmonds <apv34@fishercat.sr.unh.edu>
2014-04-21 15:17:54 -0400

    4/21/2014 -- Added debug printouts to conversion program.

--- convert.c
+++ convert.c
@@ -12,7 +12,7 @@ int main( int argc, char *argv[]  )
     int i = 0;
     int j = 0;
     
-    
+    printf( "A\n" );
     if( argc != 4 ) 
     {
         printf( "Incorrect number of passed arguments!\n" );
@@ -20,16 +20,19 @@ int main( int argc, char *argv[]  )
     }
     else
     {
-
+        printf( "B\n" );
         rows = atoi(argv[2] );
         cols = atoi(argv[3] );
+        printf( "C\n" );
         if( rows > MAX || cols > MAX )
         {
             printf( "Number of Rows/Columns Exceeds Maximum"
                 " Allowable!\n" );
             return 0;
         }
+        printf( "D\n" );
         rotate = fopen( argv[1], "r" );
+        printf( "E\n" );
         if( rotate == NULL)
         {
             printf( "File could not be opened!" );
@@ -37,20 +40,24 @@ int main( int argc, char *argv[]  )
         }
         else
         {
+            printf( "F\n" );
             printf( "R%d\nC%d\n", rows, cols );
             for( i = 0; i < rows; i++ ){
                 for( j = 0; j < cols; j++ ){
                     fscanf(rotate, "%lf", &data[i][j] );
                 }
             }
+            printf( "G\n" );
             fclose(rotate);
             rotate = fopen( argv[1], "w" );
+            printf( "H\n" );
             for( j = 0; j < cols; j++ ){
                 for( i = 0; i < rows; i++ ){
                     fprintf( rotate, "%f ", data[i][j] );
                 }
                 fprintf( rotate, "\n" );
             }
+            printf( "J\n" );
             
         }
         fclose(rotate);
