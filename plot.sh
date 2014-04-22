7d47cf2406338d447827f46822e891d4b778e249
Anthony Edmonds <apv34@fishercat.sr.unh.edu>
2014-04-22 12:10:30 -0400

    4/21/2014 -- Bash script now works for generic input to it.

--- plot.sh
+++ plot.sh
@@ -8,16 +8,16 @@ fi
 ./convert $1 $2 $3
 
 
+
 gnuplot <<- EOF
   reset
   unset key
-  unset ytics
-  unset xtics
   set yrange[-.1:2.2]
   set terminal gif size 1000,800 animate
   set output "$1.gif"
   i = 2
   n = $2
+  data = "<paste $4 rotated.txt"
   load "animate.gnuplot"
 EOF
 
